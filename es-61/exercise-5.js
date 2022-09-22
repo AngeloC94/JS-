ebd342d35f6a1b3647735a14a2866faf16affff6
AngeloC94 <tripway7777@gmail.com>
2022-09-22 14:33:22 +0200

    esercizi fino al 70

---
+++ es-61/exercise-5.js
@@ -0,0 +1,69 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+
+
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let person = persons.find((item) => item.id === id)
+      if (person) {
+        return resolve(person)
+      }
+    }, 1000);
+  });
+}
+
+
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let job = jobs.find((item) => item.id === id)
+      if (job) {
+        return resolve(job)
+      }
+    }, 1000);
+  });
+}
+const promise1 = fetchPersonById(3);
+const promise2 = fetchJobById(3);
+
+Promise.race([promise1, promise2]).then((values) => {
+  console.log(values);
+});
+
